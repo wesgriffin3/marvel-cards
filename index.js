53efba6110fe43a1743678882ac68e4b81444c37
Wes Griffin <WesGriffin@Wess-MacBook-Pro.local>
2021-01-01 18:10:53 -0600

    add search functionality with returning card

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var heroButton = $("#heroButton");
-var villainButton = $("#villainButton");
+const heroButton = $("#heroButton");
+const villainButton = $("#villainButton");
 
-var flipButtonBack = $(".flip-button-back");
-var flipButtonFront = $("#flip-button-back-to-front");
-var cardWhole = $(".card-whole");
+const flipButtonBack = $(".flip-button-back");
+const flipButtonFront = $("#flip-button-back-to-front");
+const cardWhole = $(".card-whole");
 
-var btnGroup = $(".btn-group");
+const btnGroup = $(".btn-group");
 
 heroButton.click(function () {
   cardWhole.removeClass("flipped");
@@ -23,12 +23,10 @@ $(".card-whole").hover(
 
 flipButtonBack.click(function () {
   $(this).parents($(".col-sm-4")).addClass("flipped");
-  btnGroup.style.visibility = "hidden";
 });
 
 flipButtonFront.click(function () {
   $(this).removeClass("flipped");
-  btnGroup.style.visibility = "visible";
 });
 
 villainButton.click(function () {
@@ -40,6 +38,8 @@ villainButton.click(function () {
 const form = $("form");
 
 form.submit(function () {
+  const hero = $("#searchbar").val();
+
   // creates new card container
   const newDiv = document.createElement("div");
   newDiv.className = "col-sm-4 entire-card";
@@ -62,37 +62,94 @@ form.submit(function () {
   const img = document.createElement("img");
   img.className = "card-img";
 
-  // add content to card
-  newDiv.appendChild(scene);
-  scene.appendChild(cardWhole);
-  cardWhole.appendChild(cardFace);
-  cardFace.appendChild(cardBody);
-  cardBody.appendChild(heroName);
-  cardBody.appendChild(img);
+  const par = document.createElement("p");
 
-  //   // add card to row
-  $(".third-row").append(newDiv);
+  const backSide = document.createElement("div");
+  backSide.className = "card-face card-back";
+
+  const cardBack = document.createElement("div");
+  cardBack.className = "card-body";
+
+  const backTitle = document.createElement("h3");
+
+  const imgBack = document.createElement("img");
+  imgBack.className = "card-img-back";
+
+  const backPar = document.createElement("p");
+
+  // MARVEL API
+  // KEYS:
+
+  // const publickey = "b138b6385cae17bddf0a3657ecf6e166";
+  // const privatekey = "c5ad0dcf59ecae1297f416ef21d841ee973b88df";
+
+  const userGivenHero = $("#searchbar").val();
+
+  const heroRequest = $.get(
+    "http://gateway.marvel.com/v1/public/characters?ts=1609011803321&apikey=b138b6385cae17bddf0a3657ecf6e166&hash=1e3919c6ee1bde443c8ca31977cdf615&name=" +
+      userGivenHero
+  );
+
+  const comicRequest = $.get(
+    "https://gateway.marvel.com:443/v1/public/comics?title=" +
+      userGivenHero +
+      "&ts=1609011803321&apikey=b138b6385cae17bddf0a3657ecf6e166&hash=1e3919c6ee1bde443c8ca31977cdf615"
+  );
+
+  // FRONT SIDE OF CARD
+  heroRequest.done(function (data) {
+    heroName.append(data.data.results[0].name);
+    par.append(data.data.results[0].description);
+    img.src =
+      data.data.results[0].thumbnail.path +
+      "." +
+      data.data.results[0].thumbnail.extension;
+    console.log("character data ->", data);
+    // END FRONT SIDE OF CARD
+
+    // BACK SIDE OF CARD
+
+    comicRequest.done(function (data) {
+      var randomComic =
+        data.data.results[Math.floor(Math.random() * data.data.results.length)];
+      imgBack.src =
+        randomComic.images[0].path + "." + randomComic.images[0].extension;
+      backTitle.append(randomComic.title);
+      backPar.append(randomComic.description);
+    });
+
+    // END BACK SIDE OF CARD
+
+    heroRequest.fail(function () {
+      alert("failed attempt");
+    });
+
+    // END MARVEL API
+
+    // add content to card
+    newDiv.appendChild(scene);
+    scene.appendChild(cardWhole);
+    cardWhole.appendChild(cardFace);
+    cardFace.appendChild(cardBody);
+    cardBody.appendChild(heroName);
+    cardBody.appendChild(img);
+    cardBody.appendChild(par);
+    cardWhole.appendChild(backSide);
+    backSide.appendChild(cardBack);
+    cardBack.appendChild(backTitle);
+    cardBack.appendChild(imgBack);
+    cardBack.appendChild(backPar);
+
+    //   // add card to row
+    $(".third-row").append(newDiv);
+
+    $(".card-whole").hover(
+      function () {
+        $(this).addClass("flipped");
+      },
+      function () {
+        $(this).removeClass("flipped");
+      }
+    );
+  });
 });
-
-// MARVEL API
-// KEYS
-
-// var publickey = "b138b6385cae17bddf0a3657ecf6e166";
-// var privatekey = "c5ad0dcf59ecae1297f416ef21d841ee973b88df";
-// var ts = new Date().getTime();
-// var stringToHash = ts + privatekey + publickey;
-// var hash = md5(stringToHash);
-// var baseUrl = "https://gateway.marvel.com:443/v1/public/characters";
-// var limit = 20;
-// var url =
-//   baseUrl +
-//   "?limit=" +
-//   limit +
-//   "&ts=" +
-//   ts +
-//   "&apikey=" +
-//   publickey +
-//   "&hash=" +
-//   hash;
-
-// // END MARVEL API
